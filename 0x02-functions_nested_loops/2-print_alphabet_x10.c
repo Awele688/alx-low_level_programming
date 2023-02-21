e95a594215c4852d695422fdca552bf3e6686d04
Awele688 <annappah84@gmail.com>
2023-02-21 10:13:32 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ * print_alphabet_x10 - print alph ten times
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
