d5d7463b27939bea271894354bf81b75556171cc
Awele688 <annappah84@gmail.com>
2023-03-01 15:21:48 -0800

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,18 +15,18 @@ char *cap_string(char *str)
 			index++;
 
 		if (str[index - 1] == '' ||
-			(str[index - 1] == '\t' ||
-			(str[index - 1] == '\n' ||
-			(str[index - 1] == ',' ||
-			(str[index - 1] == ';' ||
-			(str[index - 1] == '.' ||
-			(str[index - 1] == '!' ||
-			(str[index - 1] == '?' ||
-			(str[index - 1] == '"' ||
-			(str[index - 1] == '(' ||
-			(str[index - 1] == ')' ||
-			(str[index - 1] == '{' ||
-			(str[index - 1] == '}' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
 			index == 0)
 			str[index] -= 32
 
