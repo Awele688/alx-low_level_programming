9f25a312ff36c8860bf2bf6644dcea8847f8722e
Awele688 <annappah84@gmail.com>
2023-08-17 16:36:04 -0700

    update

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,43 +3,43 @@
 #include <stdlib.h>
 
 /**
- * main - generate a key depending on a username for crackme5
- * @argc: number of arguments passed
- * @argv: arguments passed to main
+ * main - write a keygen for crackme5
+ * @argc: no of args passed
+ * @argv: args passed to main
  *
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	unsigned int i, b;
-	size_t len, add;
-	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	char p[7] = "      ";
+	unsigned int j, a;
+	size_t length, sum;
+	char *m = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char o[7] = "      ";
 
 	if (argc != 2)
 	{
 		printf("Correct usage: ./keygen5 username\n");
 		return (1);
 	}
-	len = strlen(argv[1]);
-	p[0] = l[(len ^ 59) & 63];
-	for (i = 0, add = 0; i < len; i++)
-		add += argv[1][i];
-	p[1] = l[(add ^ 79) & 63];
-	for (i = 0, b = 1; i < len; i++)
-		b *= argv[1][i];
-	p[2] = l[(b ^ 85) & 63];
-	for (b = argv[1][0], i = 0; i < len; i++)
-		if ((char)b <= argv[1][i])
-			b = argv[1][i];
-	srand(b ^ 14);
-	p[3] = l[rand() & 63];
-	for (b = 0, i = 0; i < len; i++)
-		b += argv[1][i] * argv[1][i];
-	p[4] = l[(b ^ 239) & 63];
-	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
-		b = rand();
-	p[5] = l[(b ^ 229) & 63];
-	printf("%s\n", p);
+	length = strlen(argv[1]);
+	o[0] = m[(length ^ 59) & 63];
+	for (j = 0, sum = 0; j < length; j++)
+		sum += argv[1][j];
+	o[1] = m[(sum ^ 79) & 63];
+	for (j = 0, a = 1; j < length; j++)
+		a *= argv[1][j];
+	o[2] = m[(a ^ 85) & 63];
+	for (a = argv[1][0], j = 0; j < length; j++)
+		if ((char)a <= argv[1][j])
+			a = argv[1][j];
+	srand(a ^ 14);
+	o[3] = m[rand() & 63];
+	for (a = 0, j = 0; j < length; j++)
+		a += argv[1][j] * argv[1][j];
+	o[4] = m[(a ^ 239) & 63];
+	for (a = 0, j = 0; (char)j < argv[1][0]; j++)
+		a = rand();
+	o[5] = m[(a ^ 229) & 63];
+	printf("%s\n", o);
 	return (0);
 }
